647360547f6a165b1afdac67d0bfe3e1a62b1694
songku <hualixao@gmail.com>
2022-06-22 09:24:58 +0800

    cryptopp:aes_256_gcm

---
+++ aes_gcm/aes_256_gcm.cpp
@@ -0,0 +1,72 @@
+#include "aes_256_gcm.h"
+
+string aes_256_gcm_encrypt(string plain_text,CryptoPP::SecBlock<unsigned char>key,CryptoPP::SecBlock<unsigned char>iv)
+{
+    std::string cipher; //加密密文
+    std::cout << "plain text: " << plain_text << std::endl;
+    GCM< AES >::Encryption enc;  //加密对象enc
+    enc.SetKeyWithIV(key, key.size(), iv);  //设置key和iv
+
+    StringSource(plain_text, true, 
+        new AuthenticatedEncryptionFilter(enc,
+            new StringSink(cipher)
+        ) // AuthenticatedEncryptionFilter
+    ); // StringSource
+
+    std::cout << "cipher text: " << cipher << std::endl;
+    return cipher;
+}
+
+string aes_256_gcm_decrypt(string cipher_text,CryptoPP::SecBlock<unsigned char>key,CryptoPP::SecBlock<unsigned char>iv)
+{
+    std::string recovered;
+    GCM< AES >::Decryption dec;  //解密对象dec
+    dec.SetKeyWithIV(key, key.size(), iv);
+
+    StringSource s(cipher_text, true, 
+        new AuthenticatedDecryptionFilter(dec,
+            new StringSink(recovered)
+        ) // AuthenticatedDecryptionFilter
+    ); // StringSource
+    std::cout << "recovered text: " << recovered << std::endl;
+    return recovered;
+}
+
+
+void test_aes_256_gcm_encrypt_decrypt(string plain)  //测试程序，用的是prng随机生成的key和iv
+{
+    AutoSeededRandomPool prng;
+    HexEncoder encoder(new FileSink(std::cout));
+
+    //SecByteBlock key(AES::DEFAULT_KEYLENGTH); //DEFAULT_KEYLENGTH是16字节，128位的AES密钥
+    SecByteBlock key(AES::MAX_KEYLENGTH); //MAX_KEYLENGTH是32字节，256位密钥
+    SecByteBlock iv(AES::BLOCKSIZE);
+
+    prng.GenerateBlock(key, key.size());
+    prng.GenerateBlock(iv, iv.size());
+
+    std::string cipher, recovered;
+
+    std::cout << "plain text: " << plain << std::endl;
+
+    GCM< AES >::Encryption e;
+    e.SetKeyWithIV(key, key.size(), iv);
+
+    StringSource(plain, true, 
+        new AuthenticatedEncryptionFilter(e,
+            new StringSink(cipher)
+        ) // AuthenticatedEncryptionFilter
+    ); // StringSource
+
+    std::cout << "cipher text: " << cipher << std::endl;
+
+    GCM< AES >::Decryption d;
+    d.SetKeyWithIV(key, key.size(), iv);
+
+    StringSource s(cipher, true, 
+        new AuthenticatedDecryptionFilter(d,
+            new StringSink(recovered)
+        ) // AuthenticatedDecryptionFilter
+    ); // StringSource
+    std::cout << "recovered text: " << recovered << std::endl;
+}
---
+++ aes_gcm/aes_256_gcm.h
@@ -0,0 +1,31 @@
+//需要提前下载安装cryptopp库，并作为头文件包含
+#include "./cryptopp/cryptlib.h"
+#include "./cryptopp/rijndael.h"
+#include "./cryptopp/modes.h"
+#include "./cryptopp/files.h"
+#include "./cryptopp/osrng.h"
+#include "./cryptopp/hex.h"
+#include "./cryptopp/gcm.h"
+#include "./cryptopp/filters.h"
+#include <iostream>
+#include <string>
+
+using namespace CryptoPP;
+using CryptoPP::HexEncoder;
+using CryptoPP::HexDecoder;
+using CryptoPP::BufferedTransformation;
+using CryptoPP::AuthenticatedSymmetricCipher;
+using CryptoPP::StringSink;
+using CryptoPP::StringSource;
+using CryptoPP::AuthenticatedEncryptionFilter;
+using CryptoPP::AuthenticatedDecryptionFilter;
+using CryptoPP::AES;
+using CryptoPP::GCM;
+using CryptoPP::GCM_TablesOption;
+using std::string;
+using std::cout;
+using std::endl;
+
+string aes_256_gcm_encrypt(string plain_text,CryptoPP::SecBlock<unsigned char>key,CryptoPP::SecBlock<unsigned char>iv);
+string aes_256_gcm_decrypt(string cipher_text,CryptoPP::SecBlock<unsigned char>key,CryptoPP::SecBlock<unsigned char>iv);
+void test_aes_256_gcm_encrypt_decrypt(string plain);
